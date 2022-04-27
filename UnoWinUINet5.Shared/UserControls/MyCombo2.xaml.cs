4b322f40c4fa9271351c9b24130325ae4aec7cf9
Viktor Kassov <kassov@mail.ua>
2022-04-27 14:45:31 +0300

    xd

--- UnoWinUINet5.Shared/UserControls/MyCombo2.xaml.cs
+++ UnoWinUINet5.Shared/UserControls/MyCombo2.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
 
 
 
@@ -11,6 +13,49 @@ namespace UnoWinUINet5.UserControls
         public MyCombo2()
         {
             this.InitializeComponent();
+        }
+
+        private void ShowPopup(object sender, RoutedEventArgs e)
+        {
+            // open the Popup if it isn't open already 
+            if (!StandardPopup.IsOpen) { StandardPopup.IsOpen = true; }
+            PopupScrolViewer.Width = MainGrid.ActualWidth;
+            PopupBorder.PointerExited += PopupBorder_PointerExited;
+            PopupListView.SelectionChanged += PopupListView_SelectionChanged;
+
+        }
+
+        private void PopupListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MainTextBox.Text = GetSelectedValueString(PopupListView.SelectedValue);
+            PopupListView.SelectionChanged -= PopupListView_SelectionChanged;
+            PopupBorder.PointerExited -= PopupBorder_PointerExited;
+            if (StandardPopup.IsOpen) { StandardPopup.IsOpen = false; }
+        }
+
+        private string GetSelectedValueString(object selectedValue)
+        {
+            Type type = selectedValue.GetType();
+
+            if (type == typeof(string))
+            {
+                return (string)selectedValue;
+            }
+
+            if (type == typeof(TextBlock))
+            {
+                return (selectedValue as TextBlock).Text;
+            }
+
+
+            return selectedValue.ToString();
+        }
+
+        private void PopupBorder_PointerExited(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            PopupListView.SelectionChanged -= PopupListView_SelectionChanged;
+            PopupBorder.PointerExited -= PopupBorder_PointerExited;
+            if (StandardPopup.IsOpen) { StandardPopup.IsOpen = false; }
         }
     }
 }
