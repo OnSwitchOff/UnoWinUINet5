56d59138c08ba62065201be75181405ab57af576
Viktor Kassov <kassov@mail.ua>
2022-04-27 14:23:35 +0300

    erer

---
+++ UnoWinUINet5.Shared/UserControls/MyCombo2.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UnoWinUINet5.UserControls
+{
+    public sealed partial class MyCombo2 : UserControl
+    {
+        public MyCombo2()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
