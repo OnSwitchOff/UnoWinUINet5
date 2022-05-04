7e23a281a25cad733fa2b3c4523f5a3a509d3ae1
Viktor Kassov <kassov@mail.ua>
2022-05-04 17:00:28 +0300

    DSFSEDFSD

---
+++ UnoWinUINet5.Shared/UserControls/MyNavigationView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.ObjectModel;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UnoWinUINet5.UserControls
+{
+    public sealed partial class MyNavigationView : UserControl
+    {
+        public MyNavigationView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
